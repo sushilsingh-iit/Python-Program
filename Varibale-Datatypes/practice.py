9afe61a982e2f82ebf4f4d4e355ed2301553244d
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-08 00:46:42 +0530

    practice question pyhton

---
+++ Varibale-Datatypes/practice.py
@@ -0,0 +1,29 @@
+# Write a program to input 2 number and print thei sum 
+
+# answer 
+# a = int(input("Enter a 1number: "))
+# b = int(input ("Enter a 2number:"))
+# print ("sum =", a + b )
+
+
+# Question - WAP to input of a square and print its area.
+
+# wrong answer giver me 
+# a = float(input("Enter 1st Number: "))
+# b = float(input ("Enter 2nd Number:"))
+# print("square" , a**2)
+
+ # answer  right 
+
+# side = float(input("enter square side : "))
+# print("area =" , side**2)
+
+
+
+# Question 
+# WAP to input 2 int number a and b .
+# print True if . if not print false 
+
+a =int(input("enter a number:"))
+b =int(input("enter b number:"))
+print(a>=b)
