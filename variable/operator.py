1ad9cdcbcfb6393cbbc2850b77dd7333a78e78a7
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-07 22:58:01 +0530

    create operators file and learn it

---
+++ variable/operator.py
@@ -0,0 +1,54 @@
+# 1. Arithmetic Operators
+
+# Used for mathematical calculations.
+
+# Operator	Meaning	Example
+# +	Addition	10 + 5 = 15
+# -	Subtraction	10 - 5 = 5
+# *	Multiplication	10 * 5 = 50
+# /	Division	10 / 5 = 2.0
+# %	Modulus (remainder)	10 % 3 = 1
+# **	Exponent (power)	2 ** 3 = 8
+# //	Floor division	10 // 3 = 3
+
+
+a = 10
+b = 3
+print(a + b)
+print(a // b)
+
+
+
+
+# 2. Comparison (Relational) Operators
+
+# Used to compare two values (returns True or False).
+
+# Operator	Meaning
+# ==	Equal to
+# !=	Not equal to
+# >	Greater than
+# <	Less than
+# >=	Greater than or equal
+# <=	Less than or equal
+
+
+c = 10
+d = 5
+print(c > d)
+print(c == d)
+
+# 3. Logical Operators
+
+# Used to combine conditional statements.
+
+# Operator	Meaning
+# and	True if both conditions are true
+# or	True if at least one condition is true
+# not	Reverses the result
+
+x = 10
+y = 5
+print(x > 5 and y < 10)
+print(not(x == 10))
+
