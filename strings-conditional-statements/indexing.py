e92e7d1515c5ba98cd1d9b6dddca599042faf5fd
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-08 12:55:58 +0530

    craete two file index and string

---
+++ strings-conditional-statements/indexing.py
@@ -0,0 +1,19 @@
+#Sushil_Singh
+#01234567891011
+
+str = "sushil singh"
+ch = str[0]
+print(ch)
+
+# other method 
+print (str[0])
+
+
+
+# SLICING  - ACCSESSING PARTS OF A STARTING 
+# STR [STRITING-IDX : ENDING _IDX]
+# ENDING IDX NOT INCUDED
+
+
+str1 = "sushilsingh"
+print(str1 [0:6])
---
+++ strings-conditional-statements/string.py
@@ -0,0 +1,34 @@
+# strings- string is data type that stores a sequence of characters.
+# basic operations-
+# "hello"+ "world"  = "helloworld"
+# len(str)
+
+str1 = "this is string "
+str2 = 'this is string'
+str3 = """this is string """
+str4 = "this can also' a string "
+
+print(type(str1))
+print(type(str2))
+print(type(str3))
+print(type(str4))
+print(str4)
+
+
+str5 = "this is a string.\nWe are craeting it in python."
+print(str5)
+
+
+str6 = "sushil"
+str7 = "singh"
+final_str = str6 +str7 
+print(final_str)
+print(len(final_str))
+# other method 
+final_str1 = str6 +" "+str7
+print(final_str1)
+print(len (final_str1))
+
+
+# count len in stace and sopecial value 
+
