ff6288a2a0100cc807574c99feffbd4c183c6929
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-08 21:59:05 +0530

    craete tuple fil

---
+++ list-tuple/practice.py
@@ -0,0 +1,20 @@
+#WAP to ask the user to enter names of their 3 favorite movies and store them in list .
+# movie1 = input("Enter your favorite movie name:")
+# movie2 = input("Enter your favorite movie name:")
+# movie3 = input("Enter your favorite movie name:")
+# list = (movie1, movie2, movie3)
+# print (list)
+# print (type (list))
+
+
+movies = []
+mov1 = input("Enter your first movie name :")
+mov2 = input("Enter your second movie name :")
+mov3 = input("Enter your third  movie name :")
+
+movies.append(mov1)
+movies.append (mov2)
+movies.append (mov3)
+
+print(movies)
+print (type (movies))
---
+++ list-tuple/tuples.py
@@ -0,0 +1,26 @@
+# a built in data type that let us craete immutable sequences of value.
+# 
+
+tuple = (1,)  # always create a tuple with comma  other show ither tyope value 
+
+print (tuple)
+print (type(tuple))
+
+
+
+tuple1 = (1,3,2)
+print(tuple1)
+print (type (tuple1))
+
+
+
+# slicing 
+
+
+tuple3 = (3,2,1,2)
+# tup =(2,1,3,1)
+# indexing = 0,1,2,3
+# tup.index(el)  # return index first occurence tup.index(1) is 1
+# tup.count (el) # counts total occurence   tup.count(1)is 2
+
+print (tuple3.count(2))
