b1411229d4483a41718d683ead18693d21b317c4
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-10 04:27:48 +0530

    create dictionary file

---
+++ dictionary-sets/diictionary.py
@@ -0,0 +1,12 @@
+# dictionaries are used to store data values in key:value pairs 
+# They are unordered mutable(changeable) & don't allow duplicate keys 
+
+dict = {
+    "key" : "value",
+    "name" : "sushil singh",
+    "grade" : "8.1",
+    "learnig": "coding",
+    "suject" : "pythion ,c ,c++"
+
+}
+print(dict)
