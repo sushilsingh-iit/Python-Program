7d88f43c1f6aaacf81d126c1ae7979e5f6ba96d5
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-11 02:29:57 +0530

    craete loops file

---
+++ loops/whileloop.py
@@ -0,0 +1,25 @@
+# while loops 
+
+# while condyion:
+    # some work 
+
+
+# count = 1
+
+# while count <=5 :
+#     count += 1 
+#     print("Sushil")
+
+
+# print(count)
+
+
+# count ko hum iterator kahte hai
+# ess process ko niteration kahte hai
+
+# sort form 
+
+i = 1 
+while i <=5 :
+    i += 1 
+    print ("sushil")
