c5e16c28e29114848b70a17a236929ab1195d00c
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-10 23:31:06 +0530

    create a set file

---
+++ dictionary-sets/sets.py
@@ -0,0 +1,15 @@
+# sets is the collection of the unordered items.
+# each element in the set must be unique & immmutable.
+nums = { 1,2,3,4}
+set2 = {1,2,2,2}
+
+# repeated element stored only once , so it resolved to {1,2}
+
+null_set = set()  # empty set syntax 
+
+# list  under list and dict are save becouse  immutable 
+
+collection = {1,2,3,4}
+
+print (collection)
+print(type(collection))
