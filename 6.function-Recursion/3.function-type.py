b18b742c73ffe8dec7a8db475ed90d2dab0a58f4
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-13 04:07:07 +0530

    function type complete

---
+++ 6.function-Recursion/3.function-type.py
@@ -0,0 +1,18 @@
+# # built in function 
+# print()
+# len()
+# type()
+# range()
+
+print("sushil","singh")  # seprator function is autometically build in function 
+print("sushil")
+print("singh")    #end = "\n"
+
+print("sushil", end= " ") # one line second line include
+print("singh")
+
+# len function 
+def cal_prod(a=1,b=1):
+    print(a*b)
+    return a*b
+cal_prod()
