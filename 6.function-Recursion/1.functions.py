7d78632182d590bd3f40f318d2464af00d48138d
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-13 03:48:30 +0530

    complete one part in function

--- 6.function-Recursion/1.functions.py
+++ 6.function-Recursion/1.functions.py
@@ -1,1 +1,54 @@
 # function - block of statement that perform a specific task.
+# a =  4
+# b = 5
+# sum = a+b 
+# print (sum)
+# # # more lines of code
+# a = 6
+# b = 7
+# sum = a+b
+# print (sum)
+# # more lines of code 
+# a = 69
+# b = 48
+# sum = a+b
+# print (sum)  
+
+
+# repeat(redundant code in waste coder sine)
+
+# function method 
+
+# def fun_name(param1, param2..):
+# # some work
+# return 
+# fun_name (arg1, arg2..)  #function call
+
+def cal_sum(a,b):
+    sum = a+b
+    print(sum)
+    return sum
+
+cal_sum(4,5)  # function call
+# for less line code 
+cal_sum(6,7)
+# for less line code 
+cal_sum(69,48)
+
+
+# function defination 
+
+def calc_sum (c,d):  # PARAMETER 
+    return c+d
+sum = calc_sum (9,9) #FUNCTION CALL; ARGUMENT 
+print (sum)
+
+
+
+# OTHER METHOD 
+def print_hello():
+    print("hello")
+
+print_hello()
+
+print(print_hello()) # return none 
---
+++ 6.function-Recursion/2.practice.py
@@ -0,0 +1,11 @@
+#average of 3 number 
+def avg (x,y,z):
+    avg = x+y+z
+    avg1 = avg/3
+    print(avg1)
+    return avg1
+
+avg (75,85,83)
+
+
+
