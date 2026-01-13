3dd5fbd450d022f43d863b9850700c35a3ce374e
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-13 23:59:14 +0530

    practice question

---
+++ 6.function-Recursion/4.practice2.py
@@ -0,0 +1,29 @@
+#waf  to print the length of a list 9list is the parameter)
+
+nums = [ 1,2,3,4,5,6]
+hero = ["hero", "army", "farmer"]
+def print_len(list):
+    print(len(list))
+
+
+print_len(nums)
+print_len(hero)
+
+
+
+# waf to print the element of a list in a single line.(list is the parameter)
+
+par = ["para", "aura", "zenith"]
+heros = ["hero", "army", "farmer"]
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for item in list:
+        print(item, end =" ")
+
+
+print_list(heros)
+
+
+
