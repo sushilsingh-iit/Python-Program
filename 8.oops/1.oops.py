5e7b48e0fd0f702c3542cac647aa1c7dc0317dde
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-15 23:47:40 +0530

    craete a file name is class and object

--- 8.oops/1.oops.py
+++ 8.oops/1.oops.py
@@ -1,1 +1,10 @@
-#
+#to map with real world  scenarios , we started using objects in code.
+#this is called object oriented programming
+
+a = 1 
+b = 20
+sum = a+b
+print(sum)
+
+diff = a-b
+print(diff)
---
+++ 8.oops/2.class-and-object.py
@@ -0,0 +1,7 @@
+#class is a blueprint for creating objects.
+#creating class
+class student:
+    name = "sushil singh"
+# craeting object (instance)
+s1 = student()
+print(s1)
