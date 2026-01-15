3794b1105eb9354da557f09de3034a36cb6bc784
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-16 00:02:15 +0530

    craete a start learning contaructor

--- 8.oops/2.class-and-object.py
+++ 8.oops/2.class-and-object.py
@@ -4,4 +4,16 @@ class student:
     name = "sushil singh"
 # craeting object (instance)
 s1 = student()
-print(s1)
+print(s1.name)
+
+s2 = student()
+print(s2.name)
+
+
+class car:
+    color = "blue"
+    brand = "bmw"
+
+car1 = car()
+print(car1.color)
+print(car1.brand)
---
+++ 8.oops/3.constaructor.py
@@ -0,0 +1,11 @@
+# All classes have a function called _init_() , which is always executed when the
+#claass is being initiated.
+
+# creating class
+
+#class student:                                     #creating object
+#   def __init __(self ,fulname):                   # s1= student ("sushil")
+#      self.name = fullname                         #print(s1.name)
+
+#NOTE -  the self parameter is a referance to the current intance of the class, and is used to 
+#access  variables that belong to the class.
