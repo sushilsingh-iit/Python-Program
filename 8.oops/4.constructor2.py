fd006a2f767ae80a441b5318078c1b4c4d61e9f8
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-16 23:35:59 +0530

    complete one part in constaructor

---
+++ 8.oops/4.constructor2.py
@@ -0,0 +1,20 @@
+class student:
+    #defult constructors
+    def __init__(self):
+        pass 
+
+    #parameterrized constructors
+
+    def __init__(self , name , marks):
+        self.name = name
+        self.marks = marks
+        print ("adding new student in database..")
+
+s1 = student("sushil",90)
+print(s1.name,s1.marks)
+
+s2 = student("karan",89)
+print(s2.name,s2.marks)
+
+
+# run best sutable constructor at this time 
