097c7139229f873edbef9c57e426e192beb74b02
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-16 23:56:35 +0530

    complete one part in class

--- 8.oops/5.class-instance-attributes.py
+++ 8.oops/5.class-instance-attributes.py
@@ -3,3 +3,14 @@
 # 1. class attributes
 # 2.instance attributes 
 
+class student:
+    collage_name = "IIT Patna"
+    def __init__(self,name,marks):
+        self.name = name 
+        self.marks = marks
+        print("adding new student database")
+
+
+s1 = student ("sushil", 00)
+print(s1.name, s1.marks)
+        
