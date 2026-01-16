b23d3b1412b3f1f8ddf8d431974ecf1f66c8074e
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-17 00:58:55 +0530

    start method

--- 8.oops/5.class-instance-attributes.py
+++ 8.oops/5.class-instance-attributes.py
@@ -5,12 +5,13 @@
 
 class student:
     collage_name = "IIT Patna"
+    name = "singh" #class attributes 
     def __init__(self,name,marks):
-        self.name = name 
+        self.name = name  # object attributes
         self.marks = marks
         print("adding new student database")
 
 
 s1 = student ("sushil", 00)
-print(s1.name, s1.marks)
+print(s1.name, s1.marks)  # pint sushil because  = object attr > class attr
 print(s1.collage_name)
---
+++ 8.oops/6.methods.py
@@ -0,0 +1,10 @@
+#methods are functions thet belong to objects.
+
+# craeting class                                  # creating object 
+# class student:                                  #s1 = student("karan")
+#   def __init__(self,fullname):                  #s1.hello
+#       self.name = fullname
+
+# def hello (self)
+# print("hello", self.name)
+
