6fb07f1e9fe7809f342b42dcf55f3836642d9133
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-16 23:26:55 +0530

    complete one part in constaructor

--- 8.oops/3.constaructor.py
+++ 8.oops/3.constaructor.py
@@ -9,3 +9,22 @@
 
 #NOTE -  the self parameter is a referance to the current intance of the class, and is used to 
 #access  variables that belong to the class.
+
+
+class student:
+    def __init__(self,fullname,marks):
+        self.name = fullname # self is a first parameter
+        self.marks = marks 
+        print("creating new student in database..")
+
+
+s1 = student("sushil singh",98)
+print(s1.name,s1.marks)
+
+
+s2 = student("karan",99)
+print(s2.name,s2.marks)
+
+
+# self is a first parameter
+#     
