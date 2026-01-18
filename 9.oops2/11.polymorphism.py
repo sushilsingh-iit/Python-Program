ad3bce6c0b3237f4ed10146a73eb84bb2eca875a
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-19 00:33:32 +0530

    start polymorphism

---
+++ 9.oops2/11.polymorphism.py
@@ -0,0 +1,11 @@
+#operator overloading- 
+# when the same operator is allow to have different according to the context.
+
+print(1+2) #3
+print("sushil"+"singh") #concatenate
+print([1,2,3,3]+[5,6,7,8]) # merge
+
+
+# this is + is polymorphism
+
+ 
