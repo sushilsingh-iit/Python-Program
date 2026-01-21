c1f2e82f9ba659301e0264fed539a35b7a866fd7
Sushil Singh <sushilsingh.iit@gmail.com>
2026-01-21 23:21:18 +0530

    create basic qr code using python

--- 10.project/0.project.py
+++ 10.project/0.project.py
@@ -1,3 +1,15 @@
 #How to Create QR Code Generator in Python 
 
-import qrcode
+import qrcode as qr
+
+#create qr code image
+
+img = qr.make("sushil singh")
+
+#save the image 
+
+img.save("name_qr.png")
+print("QR code saved as 'name_qr'")
+
+
+
